c411e3e5ecae625263ded7faf2704cbfdae6fed0
Claudio <55328278+ClaudioBra@users.noreply.github.com>
2019-10-15 15:28:17 -0300

    Add files via upload

---
+++ Projeto.java
@@ -0,0 +1,12 @@
+package projeto;
+public class Projeto {    
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+    
+}
